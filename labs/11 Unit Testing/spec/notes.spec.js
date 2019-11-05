8b186f64504b4949e40eaa82039ee69e799ec06e
christoffertoppe <chris.mailbox85@gmail.com>
2019-11-05 22:48:27 +0200

    test update

--- labs/11 Unit Testing/spec/notes.spec.js
+++ labs/11 Unit Testing/spec/notes.spec.js
@@ -1,41 +1,62 @@
-describe("notes", function() {
-  beforeEach() {
+describe('notes', function() {
+  beforeEach(function() {
     notes.clear();
-  }
-});
-  describe("**add(note)**", function() {
-    it("should be able to add a new note", function () {
+    notes.add('one');
+    notes.add('two');
+    notes.add('three');
+    notes.add('four');
+    notes.add('five');
+  });
+
+  describe('**add(note)**', function() {
+    it('should be able to add a new note', function() {
       expect(notes.add('sixth note')).toBe(true);
+      expect(notes.count()).toBe(6);
     });
-    it("should ignore blank notes", function () {
+    it('should ignore blank notes', function() {
       expect(notes.add('')).toBe(false);
+      expect(notes.count()).toBe(5);
     });
 
+    it('should ignore whitespace', function() {
+      expect(notes.add('        ')).toBe(false);
+      expect(notes.count()).toBe(5);
+    });
 
-  });
-
-  describe("**remove(index)**", function() {
-
-  });
-
-  describe("**count()**", function() {
-
-  });
-
-  describe("**list()**", function() {
-
-  });
-
-  describe("**find(string)**", function() {
-
-  });
-
-  describe("**clear()**", function() {
-    it("is the noteslist empty?",function ()
-    {
-      expect(notes.clear()).toBe(undefined);
+    it('input should contain a String', function() {
+      expect(notes.add('        ')).toBe(false);
+      expect(notes.count()).toBe(5);
     });
   });
 
+  describe('**remove(index)**', function() {
+    it('should be able to remove index 0', function() {
+      expect(notes.remove(0)).toBe(true);
+    });
+    it('should not be able to remove index 8', function() {
+      expect(notes.remove(8)).toBe(false);
+    });
+  });
 
+  describe('**count()**', function() {
+    it('should have 5 notes', function() {
+      expect(notes.count()).toBe(5);
+    });
+  });
+
+  describe('**list()**', function() {
+
+  });
+
+  describe('**find(string)**', function() {
+    it('should find 1 value that is equal to [\'two\']', function() {
+      expect(notes.find('two')).toEqual(['two']);
+    });
+  });
+
+  describe('**clear()**', function() {
+    it('is the noteslist empty?', function() {
+      expect(notes.clear()).toBe(0);
+    });
+  });
 });
--- labs/11 Unit Testing/src/notes.js
+++ labs/11 Unit Testing/src/notes.js
@@ -1,42 +1,58 @@
 var notes = (function() {
+  var list = [];
+  var notes = [];
+  var stringList = [];
+
   return {
     add: function(note) {
-      if(!note.equals("")) {
-        notes.add(note);
+
+      note.trim();
+      note = String(note);
+      note = note.replace(" ","");
+      var item = {timestamp: Date.now(), text: note};
+      if (note.localeCompare("") === 0) {
+        return false;
+    }
+      list.push(item);
+      return true;
+    },
+
+    remove: function(index) {
+      if(list.length > index) {
+        list.slice(index);
         return true;
       }
       return false;
     },
-    remove: function(index) {},
-    count: function() {},
-    list: function() {},
-    find: function(str) {},
 
-    clear: function() {
-      notes.length = 0;
-        return notes.length;
-    }
-  }
-}());
-
-
-var notes = (function() {
-  var list = [];
-
-  return {
-    add: function(note) {
-      var item = {timestamp: Date.now(), text: note};
-      list.push(item);
-      return true;
-    },
-    remove: function(index) {},
     count: function() {
       return list.length;
     },
-    list: function() {},
-    find: function(str) {},
-    clear: function() {}
 
+    list: function() {
+      for(let i = 0; i < list.length; i++) {
+        notes.push(list[i].text);
+      }
+      return notes;
+    },
+
+    find: function(str) {
+
+      for(let i = 0; i < list.length; i++) {
+        if (list[i].text.includes(str)) {
+         stringList.push(list[i].text);
+        }
+      }
+      if (stringList.length > 0) {
+        return stringList;
+      }
+      return false;
+    },
+
+    clear: function() {
+      list.length = 0;
+      return list.length;
+    }
   }
 }());
 
